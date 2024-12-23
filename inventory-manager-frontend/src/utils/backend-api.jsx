c03f8c73a14582a5535c9048e3be7331894c46d3
Chris van Tiggelen <chrisvt@gitkraken.nl>
2024-12-23 17:51:58 +0100

    Add npm dependencies and configured react app

---
+++ inventory-manager-frontend/src/utils/backend-api.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const {VITE_BACKEND_API_URL} = import.meta.env;
+
+export const backendApi = axios.create({
+    baseURL: VITE_BACKEND_API_URL,
+    headers: {'Content-Type': 'application/json'}
+});
+
+backendApi.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
