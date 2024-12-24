8373779d27ca86fa7a42b5e5e7fe6903e756eeb8
Chris van Tiggelen <chrisvt@gitkraken.nl>
2024-12-24 12:38:04 +0100

    WIP on authentication/authorization

---
+++ inventory-manager-frontend/src/components/protected-route.jsx
@@ -0,0 +1,22 @@
+import { Navigate, Outlet } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export default function ProtectedRoute({ roles = [] }) {
+    const { token, loggedInUser } = useAuth();
+
+    if (!token) {
+        console.log("User is not authenticated. Redirecting to login page.");
+        return <Navigate to="/login" />;
+    }
+
+    if (roles.length > 0 && !roles.includes(loggedInUser?.role)) {
+        console.log("User is not authorized to view this page. Redirecting to error page.");
+        return <Navigate to="/errors/forbidden" />;
+    }
+
+    return <Outlet />;
+}
+
+ProtectedRoute.propTypes = {
+    roles: PropTypes.array
+};
--- inventory-manager-frontend/src/main.jsx
+++ inventory-manager-frontend/src/main.jsx
@@ -1,8 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './pages/root'
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <ProtectedRoute />,
+        children: [
+          {
+            index: true,
+            element: <ProductOverview />
+          }
+        ]
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
